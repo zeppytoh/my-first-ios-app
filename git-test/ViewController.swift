4a147133193e0b85c8559e9cce3b34001bf3a9d1
Victor Toh <victortoh@gmail.com>
2016-01-12 10:24:08 +0800

    Initial Commit

---
+++ git-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  git-test
+//
+//  Created by Victor Toh on 12/1/16.
+//  Copyright © 2016 Victor Toh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
